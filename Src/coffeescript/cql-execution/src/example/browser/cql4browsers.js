e30e435b10f091ca0aac8e0e4b1c3c65a6a38b1b
Chris Moesel <cmoesel@mitre.org>
2017-07-19 18:20:23 -0400

    Support for parameters in other libraries
    Revert the parameters getter that was causing issues back to its original code and update ParameterRef to take the libraryName into account (to support parameters declared in other libraries).

--- Src/coffeescript/cql-execution/src/example/browser/cql4browsers.js
+++ Src/coffeescript/cql-execution/src/example/browser/cql4browsers.js
@@ -5167,10 +5167,12 @@
     function ParameterRef(json) {
       ParameterRef.__super__.constructor.apply(this, arguments);
       this.name = json.name;
+      this.library = json.libraryName;
     }
 
     ParameterRef.prototype.exec = function(ctx) {
       var ref;
+      ctx = this.library ? ctx.getLibraryContext(this.library) : ctx;
       return (ref = ctx.getParameter(this.name)) != null ? ref.execute(ctx) : void 0;
     };
 
@@ -42787,15 +42789,8 @@
 
     Context.property("parameters", {
       get: function() {
-        var k, p, ref, v;
-        p = (ref = this.parent) != null ? ref.parameters : void 0;
-        for (k in p) {
-          v = p[k];
-          if (!(k in this._parameters)) {
-            this._parameters[k] = v;
-          }
-        }
-        return this._parameters;
+        var ref;
+        return this._parameters || ((ref = this.parent) != null ? ref.parameters : void 0);
       },
       set: function(params) {
         this.checkParameters(params);
